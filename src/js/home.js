562c0a88b0002d66004d045ea620d9dec370711c
momchilmitev <momchilmitev87@gmail.com>
2020-11-01 17:16:55 +0100

    added validating logic for the form

--- src/js/home.js
+++ src/js/home.js
@@ -1,24 +1,35 @@
+import validateRegistrationForm from './services/formValidation/validateRegistrationForm';
+
 class Home {
   constructor() {
-    this.$form = document.querySelector("#registrationForm");
-    this.$username = document.querySelector("#username");
-    this.$email = document.querySelector("#email");
-    this.$phone = document.querySelector("#phone");
-    this.$age = document.querySelector("#age");
-    this.$profession = document.querySelector("#profession");
-    this.$experience = document.querySelector("#experience");
-    this.$comment = document.querySelector("#comment");
-    this.$submit = document.querySelector("#submit");
-    this.$loadingIndicator = document.querySelector("#loadingIndicator");
+    this.$form = document.querySelector('#registrationForm');
+    this.$username = document.querySelector('#username');
+    this.$email = document.querySelector('#email');
+    this.$phone = document.querySelector('#phone');
+    this.$age = document.querySelector('#age');
+    this.$profession = document.querySelector('#profession');
+    this.$experience = document.querySelector('#experience');
+    this.$comment = document.querySelector('#comment');
+    this.$submit = document.querySelector('#submit');
+    this.$loadingIndicator = document.querySelector('#loadingIndicator');
 
-    this.$form.addEventListener("submit", (e) => {
+    this.$form.addEventListener('submit', (e) => {
       this.onFormSubmit(e);
     });
   }
 
   onFormSubmit(e) {
     e.preventDefault();
-    const formData = this.getFormValues();
+    const formValues = this.getFormValues();
+    const formStatus = validateRegistrationForm(formValues);
+
+    if (formStatus.isValid) {
+      this.clearErrors();
+      this.submitForm(formValues);
+    } else {
+      this.clearErrors();
+      this.highlightErrors(formStatus.result);
+    }
   }
 
   getFormValues() {
@@ -34,8 +45,40 @@ class Home {
       comment: this.$comment.value,
     };
   }
+
+  submitForm(formValues) {}
+
+  clearErrors() {
+    this.$username.parentElement.classList.remove('has-error');
+    this.$phone.parentElement.classList.remove('has-error');
+    this.$email.parentElement.classList.remove('has-error');
+    this.$age.parentElement.classList.remove('has-error');
+    this.$profession.parentElement.classList.remove('has-error');
+    this.$experience.parentElement.classList.remove('has-error');
+  }
+
+  highlightErrors(result) {
+    if (!result.username) {
+      this.$username.parentElement.classList.add('has-error');
+    }
+    if (!result.phone) {
+      this.$phone.parentElement.classList.add('has-error');
+    }
+    if (!result.email) {
+      this.$email.parentElement.classList.add('has-error');
+    }
+    if (!result.age) {
+      this.$age.parentElement.classList.add('has-error');
+    }
+    if (!result.profession) {
+      this.$profession.parentElement.classList.add('has-error');
+    }
+    if (!result.experience) {
+      this.$experience.parentElement.classList.add('has-error');
+    }
+  }
 }
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   new Home();
 });
